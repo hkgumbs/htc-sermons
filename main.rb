f886687f2bd6e24b8b7a50ed5857b0bccebc870a
Kofi Gumbs <h.kofigumbs@gmail.com>
2018-02-10 15:32:47 -0600

    Download sermon metadata

--- main.rb
+++ main.rb
@@ -1,14 +1,50 @@
 #!/usr/bin/env ruby
 
+require 'json'
 require 'watir'
 
+$sermons = []
+$heading = { title: '', image: nil }
+
 def run(browser)
-  browser.goto 'google.com'
-  browser.text_field(title: 'Search').set 'Hello World!'
-  browser.input(type: 'submit', value: 'Google Search').click
-  puts browser.title
+  browser.goto 'http://htcchicago.org/resources/sermons'
+  loop do
+    puts browser.url
+    browser.wait
+    get_sermons(browser)
+    break unless browser.a(id: 'next').present?
+    browser.a(id: 'next').click
+  end
 ensure
   browser.quit
 end
 
-run Watir::Browser.new(:chrome, headless: true)
+def get_sermons(browser)
+  browser.div(id: 'text').children.each do |el|
+    if el.class_name == 'series_heading'
+      $heading = {
+        title: el.h4.text,
+        image: maybe(el.img, :src),
+      }
+    else
+      $sermons << {
+        heading:  $heading[:title],
+        image:    $heading[:image] || maybe(el.img, :src),
+        title:    el.header.h3.a.text,
+        time:     el.header.p(class: 'time').text,
+        href:     el.header.ul.li(class: 'download').a.href,
+        preacher: maybe(el.header.p(class: 'meta', text: /Preacher/), :text),
+      }
+    end
+  end
+end
+
+def maybe(el, method)
+  el.exists? ? el.send(method) : nil
+end
+
+
+# MAIN
+
+Watir::Browser.new(:chrome, headless: true).tap { |browser| run browser }
+File.open('content/sermons.json', 'w') { |f| f.write($sermons.to_json) }
