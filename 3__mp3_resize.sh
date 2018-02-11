bb13315978939a9d144e6311d8890d958508b07c
Kofi Gumbs <h.kofigumbs@gmail.com>
2018-02-10 20:18:17 -0600

    Check against Squarespace criteria

--- 3__mp3_resize.sh
+++ 3__mp3_resize.sh
@@ -5,4 +5,8 @@ do
   mv $original "$original.tmp.mp3"
   ffmpeg -i "$original.tmp.mp3" -ab 48k $original
   rm "$original.tmp.mp3"
+  if [[ $(find $original -type f -size +20000000c 2>/dev/null) ]]
+  then
+      echo "WARN: $original > 20MB"
+  fi
 done
