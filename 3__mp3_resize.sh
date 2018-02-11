09cd7119a610e70397e863ce381f75008d2148cb
Kofi Gumbs <h.kofigumbs@gmail.com>
2018-02-10 20:13:32 -0600

    Bring bitrate even lower (sorry Jon)

--- 3__mp3_resize.sh
+++ 3__mp3_resize.sh
@@ -3,6 +3,6 @@
 for original in $(find $1 -type f -name "*.mp3")
 do
   mv $original "$original.tmp.mp3"
-  ffmpeg -i "$original.tmp.mp3" -ab 64k $original
+  ffmpeg -i "$original.tmp.mp3" -ab 48k $original
   rm "$original.tmp.mp3"
 done
